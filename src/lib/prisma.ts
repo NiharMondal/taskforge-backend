b8ad0f47dde17a03c58f84073174288f4d6b9a4e
nihar <developernihar@gmail.com>
2026-05-18 13:53:51 +0600

    feat: configure PrismaClient to use the Postgres adapter for database connections

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,13 +1,15 @@
 // src/lib/prisma.ts
-import { PrismaClient } from '../../generated/prisma/client';
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "../../generated/prisma/client";
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
 export const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
+    adapter: new PrismaPg(process.env.DATABASE_URL!),
     log:
-      process.env.NODE_ENV === 'development' ? ['query', 'error'] : ['error'],
+      process.env.NODE_ENV === "development" ? ["query", "error"] : ["error"],
   });
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
